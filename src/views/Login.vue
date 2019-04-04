9ed1cf88a633ebef9b2183f66e4bbc6af0e1bbb9
yuyuuuuoooo <2781871311@qq.com>
2019-04-04 12:57:09 +0800

    login over

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="login">
-    <el-form ref="form" :model="form" class="container">
+    <el-form ref="form" :model="form" class="container" :rules="rules">
       <el-form-item>
         <div class="avatar">
           <img src="../assets/avatar.jpg" alt="">
         </div>
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop="username">
         <el-input v-model="form.username" placeholder="账户" prefix-icon="myicon myicon-user"></el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop="password">
         <el-input v-model="form.password" placeholder="密码" prefix-icon="myicon myicon-key"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" class="login-btn">成功按钮</el-button>
+        <el-button type="primary" class="login-btn">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -25,6 +25,10 @@ export default {
       form: {
         username: '',
         password: ''
+      },
+      rules: {
+        username: [{ required: true, message: '请输入账户名', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
       }
     }
   }
